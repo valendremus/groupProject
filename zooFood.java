0afaf06426c6f251c21376d816a44b143996d3d2
valendremus <118015460+valendremus@users.noreply.github.com>
2022-11-11 15:32:34 -0600

    Update zooFood.java

--- zooFood.java
+++ zooFood.java
@@ -2,4 +2,19 @@
 public class zooFood {
 private String foodName;
 private int foodNum;
+
+public zooFood(String nfoodName, int nfoodNum)
+{
+  foodName = nfoodName;
+  foodNum = nfoodNum;
+}
+public String getFoodName()
+{
+  return foodName;
+}
+public int getFoodNum()
+{
+  return foodNum;
+}
+
 }
