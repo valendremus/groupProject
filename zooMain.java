851495b12e47092f231858fcbe47eabd6ff74262
valendremus <118015460+valendremus@users.noreply.github.com>
2022-11-11 16:15:33 -0600

    Update zooMain.java

--- zooMain.java
+++ zooMain.java
@@ -1,5 +1,20 @@
 
 public class zooMain {
+zooFood beef = new zooFood("beef", 50);
+zooAnimal tiger = new zooAnimal("Tiger", 6, "Jungle", beef);
+  
+zooFood bamboo = new zooFood("bamboo", 50);
+zooAnimal panda = new zooAnimal("Panda", 2, "Forest", bamboo);
+  
+zooFood fish = new zooFood("fish", 50);
+zooAnimal penguin = new zooAnimal("Penguin", 20, "Artic", fish);
+zooAnimal polarBear = new zooAnimal("Polar Bear", 2, "Artic", fish);
+  
+zooFood acaciaLeaves = new zooFood("Acacia Leaves", 50);
+zooAnimal giraffe = new zooAnimal("Giraffe", 4, "Safari", acaciaLeaves);
+  
+zooFood hay = new zooFood("hay", 50);
+zooAnimal elephan = new zooAnimal("Elephant", 5, "Safari", hay);
 //zoo animal array
 //animal search function
 }
