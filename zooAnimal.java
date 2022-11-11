48b7366a8c512bbb190344ff1183a8d2a5bb878a
bean411 <118022383+bean411@users.noreply.github.com>
2022-11-11 15:53:00 -0600

    Update zooAnimal.java

--- zooAnimal.java
+++ zooAnimal.java
@@ -3,9 +3,42 @@ public class zooAnimal {
 private String animalName;
 private int animalNum;
 private String enclosure;
-//food
-//get animal num
-//get enclosure
+private zooFood food;
+
+//constructors
+public zooAnimal(){
+  animalName = "No name inputted.";
+  animalNum = 0;
+  enclosure = "No enclosure specified.";
+}
+public zooAnimal(String name, int num, String enc){
+  animalName = name;
+  animalNum = num;
+  enclosure = enc;
+}
+  
+//setter methods
+public void setAnimalName(String n){
+  animalName = n;
+}
+public void setAnimalNum(int num){
+  animalNum = num;
+}
+public void setEnclosure(String e){
+	enclosure = e;
+}
+
+//getter methods
+public String getAnimalName(){
+  return this.animalName;
+}
+public int getAnimalNum(){
+  return this.animalNum;
+}
+public String getEnclosure(){
+  return this.enclosure;
+}
+
 //get foodName (maybe in zooFood)
 //get foodNum (maybe in zooFood)
 }
