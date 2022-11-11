f5809aac84a65d07a5a1e1cddb7acca63d67c224
valendremus <118015460+valendremus@users.noreply.github.com>
2022-11-11 15:05:11 -0600

    Update zooAnimal.java

--- zooAnimal.java
+++ zooAnimal.java
@@ -1,4 +1,11 @@
 
 public class zooAnimal {
-
+private String animalName;
+private int animalNum;
+private String enclosure;
+//food
+//get animal num
+//get enclosure
+//get foodName (maybe in zooFood)
+//get foodNum (maybe in zooFood)
 }
