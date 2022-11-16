74c0e8b112248baaad8859c6c90b08cf7aa4f1fb
valendremus <118015460+valendremus@users.noreply.github.com>
2022-11-15 23:26:20 -0600

    Update zooMain.java

--- zooMain.java
+++ zooMain.java
@@ -1,20 +1,22 @@
 
 public class zooMain {
-zooFood beef = new zooFood("beef", 50);
-zooAnimal tiger = new zooAnimal("Tiger", 6, "Jungle", beef);
+  public static void main(String[] args) {
+    zooFood beef = new zooFood("beef", 50);
+    zooAnimal tiger = new zooAnimal("Tiger", 6, "Jungle", beef);
   
-zooFood bamboo = new zooFood("bamboo", 50);
-zooAnimal panda = new zooAnimal("Panda", 2, "Forest", bamboo);
+    zooFood bamboo = new zooFood("bamboo", 50);
+    zooAnimal panda = new zooAnimal("Panda", 2, "Forest", bamboo);
   
-zooFood fish = new zooFood("fish", 50);
-zooAnimal penguin = new zooAnimal("Penguin", 20, "Artic", fish);
-zooAnimal polarBear = new zooAnimal("Polar Bear", 2, "Artic", fish);
+    zooFood fish = new zooFood("fish", 50);
+    zooAnimal penguin = new zooAnimal("Penguin", 20, "Artic", fish);
+    zooAnimal polarBear = new zooAnimal("Polar Bear", 2, "Artic", fish);
   
-zooFood acaciaLeaves = new zooFood("Acacia Leaves", 50);
-zooAnimal giraffe = new zooAnimal("Giraffe", 4, "Safari", acaciaLeaves);
+    zooFood acaciaLeaves = new zooFood("Acacia Leaves", 50);
+    zooAnimal giraffe = new zooAnimal("Giraffe", 4, "Safari", acaciaLeaves);
   
-zooFood hay = new zooFood("hay", 50);
-zooAnimal elephant = new zooAnimal("Elephant", 5, "Safari", hay);
-//zoo animal array
-//animal search function
+    zooFood hay = new zooFood("hay", 50);
+    zooAnimal elephant = new zooAnimal("Elephant", 5, "Safari", hay);
+    //zoo animal array
+    //animal search function
+  }
 }
